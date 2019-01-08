e67252f3c9c72754e6a03c57feedad54afa19bf1
Artur Krotiuk <arturkrotiuk@gmail.com>
2019-01-08 16:21:29 +0100

    Basic recipe edit

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Identifiers } from '@angular/compiler';
+import { FormGroup, FormControl } from '@angular/forms';
+import { RecipeService } from '../recipe.service';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -12,13 +15,34 @@ export class RecipeEditComponent implements OnInit {
   id: number;
   editMode: boolean = false;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService) { }
+
+  recipeForm: FormGroup;
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       this.id = +params['id'];
       this.editMode = params['id'] != null;
+      this.initForm();
     })
   }
 
+  private initForm() {
+    let recipeName='';
+    let description='';
+    let imagePath='';
+
+    if (this.editMode) {
+      const r : Recipe = this.recipeService.getRecipe(''+this.id);
+      recipeName = r.name;
+      description = r.description;
+      imagePath = r.imageUrl;
+    }
+
+    this.recipeForm = new FormGroup({
+      'name' : new FormControl(recipeName),
+      'description' : new FormControl(description),
+      'imagePath' : new FormControl(imagePath)
+    });
+  }
 }
